f3a9b858151e9b082f174362b37ef20ca5c97c89
uehara <u.hokuto@gmail.com>
2025-05-29 12:26:19 +0900

    529

--- tabelog_scraper_title.py
+++ tabelog_scraper_title.py
@@ -9,13 +9,13 @@ import lxml.html
 for pageNo in range(10):
     print('page=',pageNo+1)
     
-    bodyRes = urllib.request.urlopen('https://tabelog.com/tokyo/A1304/A130402/13041176/dtlrvwlst/COND-0/smp1/?smp=1&lc=0&rvw_part=all&PG={}'.format(str(pageNo+1)))
+    bodyRes = urllib.request.urlopen('https://tabelog.com/tokyo/A1304/A130402/13041176/dtlrvwlst/COND-0/smp1/?smp=1&lc=0&rvw_part=all&PG={}'.format(pageNo+1))
     bodyHtml = bodyRes.read()
     root = lxml.html.fromstring(bodyHtml.decode('utf-8',errors='replace'))
-
+    #rvw-item__title-target
     title_path = root.xpath("//a[@class='rvw-item__title-target']")
 
-    for title in zip(title_path):
+    for title in title_path:
         print(title.text_content())
         
 '''
